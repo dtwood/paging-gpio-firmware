df24f72ac14357dde2f737c23d69c1155b80b93b
David Wood <code@dtwood.co.uk>
2018-11-10 00:10:50 +0000

    Working LED sequence

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_halt;
 
+use cortex_m;
 use cortex_m_rt::entry;
+use tm4c129x;
 
 #[entry]
 fn main() -> ! {
+    let p = tm4c129x::Peripherals::take().unwrap();
+
+    p.SYSCTL
+        .rcgcgpio
+        .write(|w| w.r5().set_bit().r12().set_bit());
+    p.GPIO_PORTF_AHB
+        .den
+        .write(|w| w.den().bits(1 << 0 | 1 << 4));
+    p.GPIO_PORTF_AHB
+        .dir
+        .write(|w| w.dir().bits(1 << 0 | 1 << 4));
+    p.GPIO_PORTN.den.write(|w| w.den().bits(1 << 0 | 1 << 1));
+    p.GPIO_PORTN.dir.write(|w| w.dir().bits(1 << 0 | 1 << 1));
+
     loop {
-        // your code goes here
+        p.GPIO_PORTN.data.write(|w| w.data().bits(1 << 1));
+        for _ in 0..10000 {
+            cortex_m::asm::nop();
+        }
+        p.GPIO_PORTN.data.write(|w| w.data().bits(1 << 0));
+        for _ in 0..10000 {
+            cortex_m::asm::nop();
+        }
+        p.GPIO_PORTN.data.write(|w| w.data().bits(0));
+
+        p.GPIO_PORTF_AHB.data.write(|w| w.data().bits(1 << 4));
+        for _ in 0..10000 {
+            cortex_m::asm::nop();
+        }
+        p.GPIO_PORTF_AHB.data.write(|w| w.data().bits(1 << 0));
+        for _ in 0..10000 {
+            cortex_m::asm::nop();
+        }
+        p.GPIO_PORTF_AHB.data.write(|w| w.data().bits(0));
     }
 }
