cc9de61c3ea11694ed878ac85e36014d676305c0
David Wood <code@dtwood.co.uk>
2019-05-31 22:42:10 +0100

    Add gpio support

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@
 use core::fmt::Write;
 use core::panic::PanicInfo;
 use cortex_m_rt::exception;
+use heapless::consts;
+use heapless::String;
 use smoltcp::iface::{EthernetInterface, EthernetInterfaceBuilder, Neighbor, NeighborCache};
 use smoltcp::socket::{SocketHandle, SocketSet, SocketSetItem, TcpSocket, TcpSocketBuffer};
 use smoltcp::time::Instant;
@@ -102,6 +104,20 @@ static mut SERIAL: Option<
     >,
 > = None;
 
+const NUM_BUTTONS: usize = 81;
+
+/// Get port between 49152 and 65535
+fn get_ephemeral_port() -> u16 {
+    let rand = unsafe { (*tm4c129x::HIB::ptr()).rtcss.read().bits() };
+
+    let mut rand = rand + 49142;
+    while rand > 65535 {
+        rand -= 100;
+    }
+
+    rand as u16
+}
+
 #[rtfm::app(device = tm4c129x_hal::tm4c129x)]
 const APP: () = {
     static mut IFACE: EthernetInterface<'static, 'static, 'static, EthernetDevice> = ();
@@ -112,7 +128,9 @@ const APP: () = {
 
     static mut HIB: tm4c129x_hal::hib::Hib = ();
 
-    #[init(spawn = [packet_timer])]
+    static mut BUTTONS: [gpio::Pxx<gpio::Input<gpio::PullUp>>; NUM_BUTTONS] = ();
+
+    #[init(spawn = [gpio_poll, keepalive])]
     fn init(c: init::Context) -> init::LateResources {
         // static mut TCP_SERVER_RX_DATA: [u8; 1024] = [0; 1024];
         // static mut TCP_SERVER_TX_DATA: [u8; 1024] = [0; 1024];
@@ -131,9 +149,20 @@ const APP: () = {
         }
         let sc = c.device.SYSCTL.constrain();
         let mut porta = c.device.GPIO_PORTA_AHB.split(&sc.power_control);
+        let portb = c.device.GPIO_PORTB_AHB.split(&sc.power_control);
+        let portc = c.device.GPIO_PORTC_AHB.split(&sc.power_control);
+        let portd = c.device.GPIO_PORTD_AHB.split(&sc.power_control);
+        let porte = c.device.GPIO_PORTE_AHB.split(&sc.power_control);
         let portf = c.device.GPIO_PORTF_AHB.split(&sc.power_control);
+        let portg = c.device.GPIO_PORTG_AHB.split(&sc.power_control);
+        let porth = c.device.GPIO_PORTH_AHB.split(&sc.power_control);
         let portj = c.device.GPIO_PORTJ_AHB.split(&sc.power_control);
+        let portk = c.device.GPIO_PORTK.split(&sc.power_control);
+        let portl = c.device.GPIO_PORTL.split(&sc.power_control);
+        let portm = c.device.GPIO_PORTM.split(&sc.power_control);
         let portn = c.device.GPIO_PORTN.split(&sc.power_control);
+        let portp = c.device.GPIO_PORTP.split(&sc.power_control);
+        let portq = c.device.GPIO_PORTQ.split(&sc.power_control);
 
         let uart_rx_pin = porta
             .pa0
@@ -175,14 +204,14 @@ const APP: () = {
             &sc.power_control,
         );
 
+        println!("Start at {}", hib.get_millis());
+
         let mut leds: [&mut embedded_hal::digital::OutputPin; 4] = [
             &mut portn.pn1.into_push_pull_output(),
             &mut portn.pn0.into_push_pull_output(),
             &mut portf.pf4.into_push_pull_output(),
             &mut portf.pf0.into_push_pull_output(),
         ];
-        let _button1 = portj.pj0.into_pull_up_input();
-        let _button2 = portj.pj1.into_pull_up_input();
 
         for led in &mut leds {
             led.set_high();
@@ -200,7 +229,8 @@ const APP: () = {
 
         *IP_ADDRS = Some([
             IpCidr::new(IpAddress::v6(0x2001, 0xdb8, 0, 0, 0, 0, 0, 0x2), 64),
-            IpCidr::new(IpAddress::v4(192, 0, 2, 2), 24),
+            IpCidr::new(IpAddress::v6(0x2001, 0xdb8, 0, 0, 0, 0, 0, 0x3), 64),
+            // IpCidr::new(IpAddress::v4(192, 0, 2, 2), 24),
         ]);
 
         let iface = EthernetInterfaceBuilder::new(device)
@@ -226,7 +256,10 @@ const APP: () = {
         // let server_handle = socket_set.add(server_socket);
         let client_handle = socket_set.add(client_socket);
 
-        c.spawn.packet_timer(0).unwrap();
+        c.spawn.gpio_poll().unwrap();
+        c.spawn.keepalive().unwrap();
+
+        println!("Boot at {}", hib.get_millis());
 
         init::LateResources {
             IFACE: iface,
@@ -234,6 +267,91 @@ const APP: () = {
             // SERVER_HANDLE: server_handle,
             CLIENT_HANDLE: client_handle,
             HIB: hib,
+            BUTTONS: [
+                porta.pa2.into_pull_up_input().downgrade().downgrade(),
+                porta.pa3.into_pull_up_input().downgrade().downgrade(),
+                porta.pa4.into_pull_up_input().downgrade().downgrade(),
+                porta.pa5.into_pull_up_input().downgrade().downgrade(),
+                porta.pa6.into_pull_up_input().downgrade().downgrade(),
+                porta.pa7.into_pull_up_input().downgrade().downgrade(),
+                portb.pb0.into_pull_up_input().downgrade().downgrade(),
+                portb.pb1.into_pull_up_input().downgrade().downgrade(),
+                portb.pb2.into_pull_up_input().downgrade().downgrade(),
+                portb.pb3.into_pull_up_input().downgrade().downgrade(),
+                portb.pb4.into_pull_up_input().downgrade().downgrade(),
+                portb.pb5.into_pull_up_input().downgrade().downgrade(),
+                portc.pc4.into_pull_up_input().downgrade().downgrade(),
+                portc.pc5.into_pull_up_input().downgrade().downgrade(),
+                portc.pc6.into_pull_up_input().downgrade().downgrade(),
+                portc.pc7.into_pull_up_input().downgrade().downgrade(),
+                portd.pd0.into_pull_up_input().downgrade().downgrade(),
+                portd.pd1.into_pull_up_input().downgrade().downgrade(),
+                portd.pd2.into_pull_up_input().downgrade().downgrade(),
+                portd.pd3.into_pull_up_input().downgrade().downgrade(),
+                portd.pd4.into_pull_up_input().downgrade().downgrade(),
+                portd.pd5.into_pull_up_input().downgrade().downgrade(),
+                portd.pd6.into_pull_up_input().downgrade().downgrade(),
+                porte.pe0.into_pull_up_input().downgrade().downgrade(),
+                porte.pe1.into_pull_up_input().downgrade().downgrade(),
+                porte.pe2.into_pull_up_input().downgrade().downgrade(),
+                porte.pe3.into_pull_up_input().downgrade().downgrade(),
+                porte.pe4.into_pull_up_input().downgrade().downgrade(),
+                porte.pe5.into_pull_up_input().downgrade().downgrade(),
+                portf.pf1.into_pull_up_input().downgrade().downgrade(),
+                portf.pf2.into_pull_up_input().downgrade().downgrade(),
+                portf.pf3.into_pull_up_input().downgrade().downgrade(),
+                portg.pg0.into_pull_up_input().downgrade().downgrade(),
+                portg.pg1.into_pull_up_input().downgrade().downgrade(),
+                porth.ph0.into_pull_up_input().downgrade().downgrade(),
+                porth.ph1.into_pull_up_input().downgrade().downgrade(),
+                porth.ph2.into_pull_up_input().downgrade().downgrade(),
+                porth.ph3.into_pull_up_input().downgrade().downgrade(),
+                portj.pj0.into_pull_up_input().downgrade().downgrade(),
+                portj.pj1.into_pull_up_input().downgrade().downgrade(),
+                portk.pk0.into_pull_up_input().downgrade().downgrade(),
+                portk.pk1.into_pull_up_input().downgrade().downgrade(),
+                portk.pk2.into_pull_up_input().downgrade().downgrade(),
+                portk.pk3.into_pull_up_input().downgrade().downgrade(),
+                portk.pk4.into_pull_up_input().downgrade().downgrade(),
+                portk.pk5.into_pull_up_input().downgrade().downgrade(),
+                portk.pk6.into_pull_up_input().downgrade().downgrade(),
+                portk.pk7.into_pull_up_input().downgrade().downgrade(),
+                portl.pl0.into_pull_up_input().downgrade().downgrade(),
+                portl.pl1.into_pull_up_input().downgrade().downgrade(),
+                portl.pl2.into_pull_up_input().downgrade().downgrade(),
+                portl.pl3.into_pull_up_input().downgrade().downgrade(),
+                portl.pl4.into_pull_up_input().downgrade().downgrade(),
+                portl.pl5.into_pull_up_input().downgrade().downgrade(),
+                portl.pl6.into_pull_up_input().downgrade().downgrade(),
+                portl.pl7.into_pull_up_input().downgrade().downgrade(),
+                portm.pm0.into_pull_up_input().downgrade().downgrade(),
+                portm.pm1.into_pull_up_input().downgrade().downgrade(),
+                portm.pm2.into_pull_up_input().downgrade().downgrade(),
+                portm.pm3.into_pull_up_input().downgrade().downgrade(),
+                portm.pm4.into_pull_up_input().downgrade().downgrade(),
+                portm.pm5.into_pull_up_input().downgrade().downgrade(),
+                portm.pm6.into_pull_up_input().downgrade().downgrade(),
+                portm.pm7.into_pull_up_input().downgrade().downgrade(),
+                portn.pn2.into_pull_up_input().downgrade().downgrade(),
+                portn.pn3.into_pull_up_input().downgrade().downgrade(),
+                portn.pn4.into_pull_up_input().downgrade().downgrade(),
+                portn.pn5.into_pull_up_input().downgrade().downgrade(),
+                portn.pn6.into_pull_up_input().downgrade().downgrade(),
+                portn.pn7.into_pull_up_input().downgrade().downgrade(),
+                portp.pp0.into_pull_up_input().downgrade().downgrade(),
+                portp.pp1.into_pull_up_input().downgrade().downgrade(),
+                portp.pp2.into_pull_up_input().downgrade().downgrade(),
+                portp.pp3.into_pull_up_input().downgrade().downgrade(),
+                portp.pp4.into_pull_up_input().downgrade().downgrade(),
+                portp.pp5.into_pull_up_input().downgrade().downgrade(),
+                portq.pq0.into_pull_up_input().downgrade().downgrade(),
+                portq.pq1.into_pull_up_input().downgrade().downgrade(),
+                portq.pq2.into_pull_up_input().downgrade().downgrade(),
+                portq.pq3.into_pull_up_input().downgrade().downgrade(),
+                portq.pq4.into_pull_up_input().downgrade().downgrade(),
+                // portj.pj0.into_pull_up_input().downgrade().downgrade(),
+                // portj.pj1.into_pull_up_input().downgrade().downgrade(),
+            ],
         }
     }
 
@@ -245,10 +363,7 @@ const APP: () = {
 
         loop {
             let _ = socket_set.lock(|socket_set| {
-                iface.poll(
-                    socket_set,
-                    Instant::from_millis(hib.lock(|hib| hib.get_millis())),
-                )
+                hib.lock(|hib| iface.poll(socket_set, Instant::from_millis(hib.get_millis())))
             });
 
             // if let Some(delay) = c.resources.IFACE.poll_delay(
@@ -264,52 +379,109 @@ const APP: () = {
     }
 
     #[task(
-        schedule = [packet_timer],
-        resources=[SOCKET_SET, CLIENT_HANDLE, HIB],
+        schedule=[keepalive],
+        resources=[SOCKET_SET, CLIENT_HANDLE],
+        priority=1,
     )]
-    fn packet_timer(mut c: packet_timer::Context, i: i64) {
-        static mut DID_CONNECT: bool = false;
+    fn keepalive(c: keepalive::Context) {
+        let mut socket_set = c.resources.SOCKET_SET;
+        let mut client_handle = c.resources.CLIENT_HANDLE;
 
-        if c.resources.HIB.get_millis() > i * 1000 {
-            let mut socket = c
-                .resources
-                .SOCKET_SET
-                .get::<TcpSocket>(*c.resources.CLIENT_HANDLE);
-            if !socket.is_open() {
-                if !*DID_CONNECT {
+        socket_set.lock(|socket_set| {
+            client_handle.lock(|client_handle| {
+                let mut socket = socket_set.get::<TcpSocket>(*client_handle);
+
+                if !socket.is_open() {
                     socket
                         .connect(
                             (IpAddress::v6(0x2001, 0xdb8, 0, 0, 0, 0, 0, 1), 49280),
-                            (IpAddress::Unspecified, 65000),
+                            (IpAddress::Unspecified, get_ephemeral_port()),
                         )
                         .unwrap();
-                    *DID_CONNECT = true;
                 }
-            }
 
-            if socket.can_send() {
-                if i % 2 == 0 {
-                    socket.send_slice(b"SET channel0 \"ON\"\r").unwrap();
-                } else {
-                    socket.send_slice(b"SET channel0 \"OFF\"\r").unwrap();
-                }
-            }
+                let _ = socket.send_slice(b"\r");
+            })
+        });
 
-            if socket.can_recv() {
-                println!("got {:?}", socket.recv(|buffer| (buffer.len(), buffer)));
-            }
+        c.schedule
+            .keepalive(c.scheduled + (120_000_000).cycles())
+            .unwrap();
+    }
 
-            c.schedule
-                .packet_timer(c.scheduled + (10000000).cycles(), i + 1)
-                .unwrap();
-        } else {
-            c.schedule
-                .packet_timer(c.scheduled + (10000000).cycles(), i)
+    #[task(
+        schedule = [gpio_poll],
+        resources=[SOCKET_SET, CLIENT_HANDLE, BUTTONS, HIB],
+        priority=2,
+    )]
+    fn gpio_poll(mut c: gpio_poll::Context) {
+        static mut OLD_STATE: [Option<bool>; NUM_BUTTONS] = [None; NUM_BUTTONS];
+        static mut HAS_SENT: bool = false;
+
+        let hib = c.resources.HIB;
+        let mut socket = c
+            .resources
+            .SOCKET_SET
+            .get::<TcpSocket>(*c.resources.CLIENT_HANDLE);
+
+        if !socket.is_open() {
+            socket
+                .connect(
+                    (IpAddress::v6(0x2001, 0xdb8, 0, 0, 0, 0, 0, 1), 49280),
+                    (IpAddress::Unspecified, get_ephemeral_port()),
+                )
                 .unwrap();
         }
+
+        for (index, (button, old_state)) in c
+            .resources
+            .BUTTONS
+            .iter()
+            .zip(OLD_STATE.iter_mut())
+            .enumerate()
+        {
+            let new_state = button.is_low();
+
+            if Some(new_state) != *old_state {
+                if let Ok(true) = socket.send(|buffer| {
+                    let mut s = String::<consts::U64>::new();
+                    write!(
+                        s,
+                        "SET channel{} \"{}\" @ {}\r",
+                        index,
+                        if new_state { "ON" } else { "OFF" },
+                        hib.get_millis()
+                    )
+                    .unwrap();
+                    let bytes = s.as_bytes();
+
+                    if buffer.len() > bytes.len() {
+                        buffer[0..bytes.len()].copy_from_slice(bytes);
+                        (bytes.len(), true)
+                    } else {
+                        (0, false)
+                    }
+                }) {
+                    *old_state = Some(new_state);
+                    if !*HAS_SENT {
+                        println!("First packet at {}", hib.get_millis());
+                    }
+                    *HAS_SENT = true;
+                }
+            }
+        }
+
+        if socket.can_recv() {
+            dbg!(socket.recv(|buffer| (buffer.len(), buffer))).unwrap();
+        }
+
+        c.schedule
+            .gpio_poll(c.scheduled + (10_000_000).cycles())
+            .unwrap();
     }
 
     extern "C" {
+        fn UART6();
         fn UART7();
     }
 };
